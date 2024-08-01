2d1475e47675d7c163eb719cf8cba5cf06ec2d3e
Toby Gethin <toby@launcestonit.com.au>
2024-08-01 15:44:06 +1000

    Add install menu

---
+++ functions/Show-InstallMenu.ps1
@@ -0,0 +1,25 @@
+function Show-InstallMenu {
+    Write-Host "Please select an option:"
+    Write-Host "0) Main Menu"
+    Write-Host "1) Comet"
+    Write-Host "2) Sophos ( or silent install exe)"
+    Write-Host "Enter a number (0-2):"
+    
+    $choice = Read-Host
+    
+    switch ($choice) {
+        0 {
+            Show-MainMenu
+        }
+        1 {
+            Deploy-CometBackup
+        }
+        2 {
+            Deploy-Sophos
+        }
+        default {
+            Write-Host "Invalid selection. Please enter a number between 1 and 2."
+            Show-Menu
+        }
+    }
+}
