90585b63e19ac6963cc44936684570182a00c08f
Toby Gethin <toby@launcestonit.com.au>
2024-08-01 15:43:32 +1000

    fix logo file name

---
+++ functions/Show-Logo.ps1
@@ -0,0 +1,27 @@
+Function Show-Logo {
+    <#
+
+    .SYNOPSIS
+        Prints the logo
+    #>
+
+
+                                                                                                             
+                                                                                                             
+Write-Host "lllllll   iiii          tttt                                                                 lllllll   iiii  "
+Write-Host "l:::::l  i::::i      ttt:::t                                                                 l:::::l  i::::i "
+Write-Host "l:::::l   iiii       t:::::t                                                                 l:::::l   iiii  "
+Write-Host "l:::::l              t:::::t                                                                 l:::::l         "
+Write-Host " l::::l iiiiiiittttttt:::::ttttttt    uuuuuu    uuuuuu                       cccccccccccccccc l::::l iiiiiii "
+Write-Host " l::::l i:::::it:::::::::::::::::t    u::::u    u::::u                     cc:::::::::::::::c l::::l i:::::i "
+Write-Host " l::::l  i::::it:::::::::::::::::t    u::::u    u::::u                    c:::::::::::::::::c l::::l  i::::i "
+Write-Host " l::::l  i::::itttttt:::::::tttttt    u::::u    u::::u   --------------- c:::::::cccccc:::::c l::::l  i::::i "
+Write-Host " l::::l  i::::i      t:::::t          u::::u    u::::u   -:::::::::::::- c::::::c     ccccccc l::::l  i::::i "
+Write-Host " l::::l  i::::i      t:::::t          u::::u    u::::u   --------------- c:::::c              l::::l  i::::i "
+Write-Host " l::::l  i::::i      t:::::t          u::::u    u::::u                   c:::::c              l::::l  i::::i "
+Write-Host " l::::l  i::::i      t:::::t    ttttttu:::::uuuu:::::u                   c::::::c     ccccccc l::::l  i::::i "
+Write-Host "l::::::li::::::i     t::::::tttt:::::tu:::::::::::::::uu                 c:::::::cccccc:::::cl::::::li::::::i"
+Write-Host "l::::::li::::::i     tt::::::::::::::t u:::::::::::::::u                  c:::::::::::::::::cl::::::li::::::i"
+Write-Host "l::::::li::::::i       tt:::::::::::tt  uu::::::::uu:::u                   cc:::::::::::::::cl::::::li::::::i"
+Write-Host "lllllllliiiiiiii         ttttttttttt      uuuuuuuu  uuuu                     cccccccccccccccclllllllliiiiiiii"
+}
