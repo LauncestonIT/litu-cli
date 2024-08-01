0e64a1f784378f6cd40700b9bd51f68e4d2cf933
Toby Gethin <toby@launcestonit.com.au>
2024-08-01 15:44:24 +1000

    Add deploy sophos function

---
+++ functions/Deploy-Sophos.ps1
@@ -0,0 +1,33 @@
+function Deploy-Sophos {
+
+    <#
+    .SYNOPSIS
+        Downloads Sophos from URL and Installs any exe installer silently.
+    #>
+
+    # Get the user input
+    $URL = Read-Host "Enter Sophos exe URL"
+
+    if (-not $URL) {
+        throw "No URL provided."
+    }
+
+    $exePath = "$env:TEMP\SophosSetup.exe"
+    
+    # Download the zip file
+    try {
+        Write-Host "Downloading Sophos from $url..."
+        Invoke-WebRequest -Uri $url -OutFile $exePath
+    }
+    catch {
+        throw "Failed to download the file from $url. $_"
+    }
+    
+    if (-Not (Test-Path $exePath)) {
+        throw "Sophos installer are missing."
+    } else {
+        # Start the installer with silent install flag
+        Write-Host "Quietly running installer..."
+        Start-Process -FilePath $exePath -ArgumentList "--quiet" -Wait
+    }
+}
