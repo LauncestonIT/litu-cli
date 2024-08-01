e1722a9bea521da4d52413a6bdcf591672ffd247
Toby Gethin <toby@launcestonit.com.au>
2024-08-01 16:46:52 +1000

    Add config menu

---
+++ functions/Show-ConfigMenu.ps1
@@ -0,0 +1,28 @@
+function Show-ConfigMenu {
+    Clear-Host
+    Write-Host "Please select an option:"
+    Write-Host "0) Return to Main Menu"
+    Write-Host "1) Change Hostname"
+    Write-Host "Enter a number (1-3):"
+    
+    $choice = Read-Host
+    
+    switch ($choice) {
+        0 {
+            Show-MainMenu
+        }
+        1 {
+            Set-Hostname
+        }
+        2 {
+            Show-ConfigMenu
+        }
+        3 {
+            Show-ConfigMenu
+        }
+        default {
+            Write-Host "Invalid selection. Please enter a number between 1 and 4."
+            Show-Menu
+        }
+    }
+}
