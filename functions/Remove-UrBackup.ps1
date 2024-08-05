303954191fe1d124e0793a0c3bb16ce9feeaba2f
Toby Gethin <toby@launcestonit.com.au>
2024-08-05 15:40:31 +1000

    Fix Remove-Backup function

--- functions/Remove-UrBackup.ps1
+++ functions/Remove-UrBackup.ps1
@@ -1,19 +1,17 @@
 function Remove-UrBackup {
     Clear-Host
 
-    $UrBackup = "C:\Program Files\UrBackup"
-    $Exe = "C:\Program Files\UrBackup\Uninstall.exe"
+    $UrBackup = "C:\Program Files\UrBackup\Uninstall.exe"
 
-    Write-Host "Checking if UrBackup is installed.."
     if (Test-Path -Path $UrBackup -PathType Container) {
         Write-Host "Starting uninstall.."
-        Start-Process -FilePath $Exe -ArgumentList "/S" -Wait
+        Start-Process -FilePath $UrBackup -ArgumentList "/S" -Wait
         
         Write-Host "Successfully uninstalled, returning to menu.."
         Start-Sleep -Seconds 3
         Show-MainMenu
     } else {
-        Write-Host "Backblaze isn't installed, returning to main menu.."
+        Write-Host "UrBackup isn't installed, returning to main menu.."
         Start-Sleep -Seconds 3
         Show-MainMenu
     }
