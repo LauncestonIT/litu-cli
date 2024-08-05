9768de0e14975e0f3e4a8631a568368349ecd313
Zaide <zaide@launcestonit.com.au>
2024-08-05 12:56:10 +1000

    Undo commit 345ab02

--- functions/Remove-Backblaze.ps1
+++ functions/Remove-Backblaze.ps1
@@ -8,7 +8,8 @@ function Remove-Backblaze {
     if (Test-Path -Path $Path -PathType Container) {
         Write-Host "Starting uninstall.."
         Start-Process -FilePath $Exe -ArgumentList "-douninstall -nogui" -Wait
-        #Remove-Item -Path $Path -Recurse -Force
+        Stop-Process -Name "bzbui.exe" -Force
+        Remove-Item -Path $Path -Recurse -Force
         Write-Host "Successfully uninstalled, returning to menu.."
         Start-Sleep -Seconds 3
         Show-MainMenu
