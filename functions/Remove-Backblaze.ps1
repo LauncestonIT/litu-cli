7e54a7b5bafd42c2e3cd0106802fceb4a49ccc35
Zaide <zaide@launcestonit.com.au>
2024-08-05 10:06:06 +1000

    Add Remove-Backblaze Function

---
+++ functions/Remove-Backblaze.ps1
@@ -0,0 +1,20 @@
+function Remove-Backblaze {
+    Clear-Host
+
+    $Path = "C:\Program Files (x86)\Backblaze"
+    $Exe = "C:\Program Files (x86)\Backblaze\bzdoinstall.exe"
+
+    Write-Host "Checking if Backblaze is installed.."
+    if (Test-Path -Path $Path -PathType Container) {
+        Write-Host "Starting uninstall.."
+        Start-Process -FilePath $Exe -ArgumentList "/douninstall /nogui" -Wait
+        Remove-Item -Path $Path -Recurse -Force
+        Write-Host "Successfully uninstalled, returning to menu.."
+        Start-Sleep -Seconds 3
+        Show-MainMenu
+    } else {
+        Write-Host "Backblaze isn't installed, returning to main menu.."
+        Start-Sleep -Seconds 3
+        Show-MainMenu
+    }
+}
--- functions/Show-MainMenu.ps1
+++ functions/Show-MainMenu.ps1
@@ -5,7 +5,8 @@ function Show-MainMenu {
     Write-Host "1) Install"
     Write-Host "2) Config"
     Write-Host "3) Audit"
-    Write-Host "Enter a number (1-3):"
+    Write-Host "4) Remove Backblaze"
+    Write-Host "Enter a number (1-4):"
     
     $choice = Read-Host
     
@@ -18,6 +19,9 @@ function Show-MainMenu {
         }
         3 {
             Send-ComputerInfoToHudu
+        }
+        4 {
+            Remove-Backblaze
         }
         default {
             Write-Host "Invalid selection. Please enter a number between 1 and 4."
