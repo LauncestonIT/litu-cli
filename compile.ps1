d8886eaad200c7be6d1c41ad0a5f647b27f3810c
Toby Gethin <toby@launcestonit.com.au>
2024-09-27 14:31:17 +1000

    Update compile script to look in menu folder

--- compile.ps1
+++ compile.ps1
@@ -1,54 +1,41 @@
 $scriptname = "litu-cli.ps1"
 
-function Update-Progress {
-    param (
-        [Parameter(Mandatory, position=0)]
-        [string]$StatusMessage,
-
-	[Parameter(Mandatory, position=1)]
-	[ValidateRange(0,100)]
-        [int]$Percent,
-
-	[Parameter(position=2)]
-	[string]$Activity = "Compiling"
-    )
-
-    Write-Progress -Activity $Activity -Status $StatusMessage -PercentComplete $Percent
-}
-
-
 # Create the script in memory.
-Update-Progress "Pre-req: Allocating Memory" 0
 $script_content = [System.Collections.Generic.List[string]]::new()
 
-Update-Progress "Adding: Header" 5
+# Add Header
 $script_content.Add($header)
 $script_content.Add("")
 
-Update-Progress "Adding: Version" 10
+# Add Version
 $start_content = Get-Content .\scripts\start.ps1 -Raw
 $start_content = $start_content.replace('#{replaceme}', "$(Get-Date -Format yy.MM.dd)")
 $script_content.Add($start_content)
 $script_content.Add("")
 
-Update-Progress "Adding: Functions" 20
+# Add Menu Content
+Get-ChildItem .\menus -Recurse -File | ForEach-Object {
+    $script_content.Add($(Get-Content $_.FullName -Raw))
+    $script_content.Add("")
+}
+
+# Add Functions
 Get-ChildItem .\functions -Recurse -File | ForEach-Object {
     $script_content.Add($(Get-Content $_.FullName -Raw))
     $script_content.Add("")
 }
 
-Update-Progress "Adding: Main Script Content" 75
+# Add Main Script Content
 $script_content.Add($(Get-Content .\scripts\main.ps1 -Raw))
 
+# Save the compiled script
 Set-Content -Path $scriptname -Value ($script_content -join "`r`n") -Encoding ascii
-Write-Progress -Activity "Compiling" -Completed
 
-if ($run){
+# Run the script if the $run flag is set
+if ($run) {
     try {
         Start-Process -FilePath "pwsh" -ArgumentList ".\$scriptname"
     } catch {
         Start-Process -FilePath "powershell" -ArgumentList ".\$scriptname"
     }
 }
-
-
